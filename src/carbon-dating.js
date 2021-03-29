a067270a386f3c74a7dd3d4c470afd1d2ccdeebf
alina-str <al-strukova@mail.ru>
2021-03-30 00:29:38 +0300

    add: solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -11,16 +11,4 @@ return false;
 }
 let k = Math.ceil((Number(HALF_LIFE_PERIOD) * Math.log(MODERN_ACTIVITY / (Number(sampleActivity)))) / 0.693);
 return k;
-}
- 
-module.exports = class DepthCalculator {
-calculateDepth(arr) {
-let result = 1;
-arr = arr.filter(element => Array.isArray(element));
-if (arr.length > 0) {
-arr = arr.flat();
-result += this.calculateDepth(arr);
-}
-return result;
-}
-}
+};
