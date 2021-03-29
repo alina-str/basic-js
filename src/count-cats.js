d4f4dda3cdd8dc2bb7a05da387df5e1b5587e87f
alina-str <al-strukova@mail.ru>
2021-03-29 21:24:14 +0300

    add: solution

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,14 +1,10 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function countCats(matrix) {
-    let kstr=1, kstolb=0, k=0;
+    let k=0;
      for (let i=0; i<matrix.length; i++)
-    if (matrix[i]=='\n')
-    kstr++;
-    kstolb=matrix.length/kstr;
-    for (let i=0; i<kstr; i++)
-    for (let j=0; i<kstolb; j++)
-    if(matrix[i][j]=="^^") k++;
+     for (let j=0; j<matrix[j].length; j++)
+    if(matrix[i]=="^^") k++;
     return k;
     
 };
