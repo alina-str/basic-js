771e46abb26ea940c27caafbba15345c469723e6
alina-str <al-strukova@mail.ru>
2021-03-29 23:04:00 +0300

    add: solution

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,22 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+ let str="";
+ let mass=[];
+ let n=0;
+ for (let i=0; i<members.length; i++)
+ {
+   if(typeof members[i] == "string") {
+     for(let j=0; j<members[i].length; j++) {
+       if (members[i][j]!=' ')  {mass[n]=members[i][j].toUpperCase(); n++; break;
+     }
+    }
+  }
+}
+ mass.sort();
+ for (let i=0; i<mass.length;i++)
+ {str=str+mass[i];}
+ str=str.toUpperCase();
+ return str;
+}
