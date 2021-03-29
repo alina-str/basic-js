d10ff19e26bd12b5ae2f1f50f8908614c5ee4c12
alina-str <al-strukova@mail.ru>
2021-03-30 00:20:11 +0300

    add: solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,9 +1,27 @@
 const CustomError = require("../extensions/custom-error");
 
-const MODERN_ACTIVITY= 15; 
-const HALF_LIFE_PERIOD= 5730;
+const MODERN_ACTIVITY = 15;
+const HALF_LIFE_PERIOD = 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+if (typeof sampleActivity != "string") return false;
+if (Number(sampleActivity) > 15 || Number(sampleActivity) <= 0) return false;
+if (isNaN(sampleActivity)) {
+return false;
+}
+let k = Math.ceil((Number(HALF_LIFE_PERIOD) * Math.log(MODERN_ACTIVITY / (Number(sampleActivity)))) / 0.693);
+return k;
+};
+ 
+module.exports = class DepthCalculator {
+calculateDepth(arr) {
+let result = 1;
+arr = arr.filter(element => Array.isArray(element));
+if (arr.length > 0) 
+{
+arr = arr.flat();
+result += this.calculateDepth(arr);
+}
+return result;
+}
 };
